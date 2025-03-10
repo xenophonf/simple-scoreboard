38274f1d61fd2e8e28ddc2517464773f248e9728
Matthew X. Economou <xenophon@irtnog.org>
2025-03-10 18:56:45 +0000

    feat: store players' scores in a DynamoDB table

--- main.tf
+++ main.tf
@@ -22,3 +22,15 @@ resource "aws_cloudwatch_log_group" "this" {
   name              = "/aws/lambda/${var.STACK_NAME}"
   retention_in_days = 3
 }
+
+# Store players' scores in a DynamoDB table.
+resource "aws_dynamodb_table" "this" {
+  name         = "simple-scoreboard"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "name"
+
+  attribute {
+    name = "name"
+    type = "S"
+  }
+}
