cbed4f8e94feadc75b8b000e6e9dfa65ef94f82e
Matthew X. Economou <xenophon@irtnog.org>
2025-03-31 19:06:29 +0000

    refactor: adopt the Python src packaging flow

--- main.tf
+++ main.tf
@@ -8,7 +8,6 @@ terraform {
     }
   }
 
-  # NOTE: Don't forget to change the bucket and table names!
   backend "s3" {
     bucket         = "s3-tfstate-statestoragebucket-ccgaowlpq1jt"
     key            = "simple-scoreboard.tfstate"
@@ -98,7 +97,7 @@ resource "aws_lambda_function" "this" {
   role             = aws_iam_role.this.arn
   filename         = "lambda-function.zip"
   source_code_hash = filebase64sha256("lambda-function.zip")
-  handler          = "lambda_function.lambda_handler"
+  handler          = "simple_scoreboard.lambda_handler"
   runtime          = "python3.12"
   architectures    = ["x86_64"]
 
